a813fd2c7f34e1a7a0382567a8caf946e9dc7e86
Ramiro Mendez <90067776+rmendez314@users.noreply.github.com>
2022-12-08 14:09:53 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Experience from './pages/Experience';
+import About from './pages/About';
 
 function App() {
   return (
     <>
       <Router>
-        <NavBar>
-          <Switch>
-            <Route path="/" />
-          </Switch>
-        </NavBar>
+        <NavBar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/experience' component={Experience} />
+          <Route path='/about' component={About} />
+        </Switch>
       </Router>
     </>
   );
