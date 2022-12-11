e2f6e6d87aed0c262af3b0243d3ca5eb78056a89
Ramiro Mendez <90067776+rmendez314@users.noreply.github.com>
2022-12-11 10:40:15 -0600

    fixed image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ function Home() {
   return (
     <>
       <div classname='home-container'>
-        <div classname='home-image'>
-          <img src='./gradpic1.jpg' alt='Ramiro Mendez Grad Pic1'/>
+        <div classname='home-image-container'>
+          <img src='./gradpic1.jpg' alt='Ramiro Mendez Grad Pic1' className='home-image'/>
         </div>
         <h1>Ramiro Mendez</h1>
       </div>
