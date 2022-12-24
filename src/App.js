1db3c24a51b14711fdc094d85de8adb0077c8783
Ramiro Mendez <eyi617@my.utsa.edu>
2022-12-23 23:06:28 -0600

    added scroll feature

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import './App.css';
-import NavBar from './components/NavBar/NavBar.js';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useRef } from 'react';
+// import NavBar from './components/NavBar/NavBar.js';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Experience from './pages/Experience';
 import About from './pages/About';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop.js';
 
 function App() {
 
-  // const ref = useRef(null);
+  const home = useRef(null);
+  const about = useRef(null);
+  const experience = useRef(null);
+  const contact = useRef(null);
 
-  // const handleClick = () => {
-
-  // }
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: 'smooth'
+    });
+  }
 
   return (
-    <>
+    <div className="App">
+      <ScrollToTop />
+      <div className="hero">
+        <ul>
+          <li onClick={() => scrollToSection(home)} className="link">
+            Home
+          </li>
+          <li onClick={() => scrollToSection(about)} className="link">
+            About
+          </li>
+          <li onClick={() => scrollToSection(experience)} className="link">
+            Experience
+          </li>
+          <li onClick={() => scrollToSection(contact)} className="link">
+            Contact
+          </li>
+        </ul>
+      </div>
+      <div ref={home}>
+        <Home />
+      </div>
+      <div ref={about}>
+        <About />
+      </div>
+      <div ref={experience}>
+        <Experience />
+      </div>
+      <div ref={contact}>
+        <Contact />
+      </div>
+    </div>
+    );
       {/* <Router>
         <NavBar />
         <Switch>
@@ -25,15 +64,6 @@ function App() {
           <Route path='/about' component={About} />
         </Switch>
       </Router> */}
-      <Router>
-        <NavBar />
-      </Router>
-      <Home />
-      <About />
-      <Experience />
-      <Contact />
-    </>
-  );
 }
 
 export default App;
