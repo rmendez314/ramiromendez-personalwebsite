af2cc2a011746ed26f2fc8ef18f6e13ab97ed8c8
Ramiro Mendez <90067776+rmendez314@users.noreply.github.com>
2022-12-28 21:14:40 -0600

    completed about component

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import Footer from './components/footer/Footer';
 
-
-
 function App() {
 
   return (
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import Me from '../../Assets/bigbendpic.jpeg'
+import { FaUserClock } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 import './About.css'
 
 function About() {
   return (
     <section id='about'>
-      About
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me__img'>
+            <img src={Me} alt='Ramiro Mendez Grad Pic' />
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaUserClock className='about__icon'/>
+              <h5>Experience</h5>
+              <small>6+ Months Professional</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Experience</h5>
+              <small>6+ Months Professional</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Experience</h5>
+              <small>6+ Months Professional</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Debitis facere veritatis numquam deleniti repudiandae 
+            unde repellendus assumenda. Aperiam animi rerum dolorum nostrum? 
+            Pariatur saepe culpa, optio asperiores aliquam modi veniam.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
